a59ab1969d607a08bdddadfc6d28bece83671d8c
berniceyeo <berniceyeosuping@gmail.com>
2022-07-22 17:19:17 +0800

    feat: adding input to change page numbers

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -7,20 +7,30 @@ import "../index.css";
 
 export function Carousel({ slides }: { slides: any }) {
   const [range, setRange] = useState(0);
+  const [page, setPage] = useState(1);
   const allSlides = [...slides];
   const length = allSlides.length;
+  // const numOfPages = Math.ceil(length / 3);
 
   function nextItem() {
-    //prevents movements once the item is on the last page
-    setRange(range === length - 3 ? range : range + 1);
+    //prevents movements once the item is within the last umbrella, if the last item is in the first position, prevent movement
+    //flush left for last item for pages
+    setRange(range >= length - 3 ? range : range + 1);
+    // if new position is the 0 index of the frame, means its a new page (from remainder 2 to 0)
+    const newPosition = (range + 1) % 3;
+
+    if (range + 1 === length - 3 || newPosition === 0) {
+      setPage(page + 1);
+    }
   }
 
   function previousItem() {
-    setRange(range === 0 ? range : range - 1);
-  }
-
-  if (!Array.isArray(allSlides) || length <= 0) {
-    return null;
+    setRange(range <= 0 ? range : range - 1);
+    const newPosition = (range - 1) % 3;
+    if (range - 1 === length - 4 || newPosition === 2) {
+      console.log("minus one page");
+      setPage(page - 1);
+    }
   }
 
   let divStyle = {
@@ -36,7 +46,13 @@ export function Carousel({ slides }: { slides: any }) {
         <RightButton click={nextItem} />
       </div>
 
-      <PageButtonGroup range={range} setRange={setRange} slides={slides} />
+      <PageButtonGroup
+        range={range}
+        setRange={setRange}
+        page={page}
+        setPage={setPage}
+        slides={slides}
+      />
     </div>
   );
 }
--- src/components/PageButtonGroup.tsx
+++ src/components/PageButtonGroup.tsx
@@ -6,23 +6,43 @@ export function PageButtonGroup({
   slides,
   range,
   setRange,
+  page,
+  setPage,
 }: {
   slides: any;
   range: number;
   setRange: any;
+  page: number;
+  setPage: any;
 }) {
   const allSlides = [...slides];
   const length = allSlides.length;
-  const numOfPages = Math.floor(length / 3);
+  const numOfPages = Math.ceil(length / 3);
 
   function leftPage() {
-    setRange(range <= 0 ? 0 : range - 3);
-    // setPage(page === 1 ? page : page - 1);
+    setPage(page === 1 ? page : page - 1);
+    // if the page is the first page, then to not move left
+    const newRange = page <= 1 ? 0 : (page - 1) * 3 - 3;
+    setRange(newRange);
   }
 
   function rightPage() {
-    setRange(range >= length - 1 ? range : range + 3);
-    // setPage(page === numOfPages ? page : page + 1);
+    setPage(page === numOfPages ? page : page + 1);
+    //if the page is the last page, to flush right for the cards
+    const newRange = page + 1 >= numOfPages ? length - 3 : (page + 1) * 3 - 3;
+    setRange(newRange);
+  }
+
+  function changePage(event: React.ChangeEvent<HTMLInputElement>) {
+    const newPage = Number(event.currentTarget.value);
+    //if the inputted page is not beyond the listed range
+    if (newPage > 0 && newPage <= numOfPages) {
+      setPage(newPage);
+      const newRange = newPage === numOfPages ? length - 3 : newPage * 3 - 3;
+      setRange(newRange);
+    } else {
+      event.currentTarget.value = `${page}`;
+    }
   }
 
   return (
@@ -30,7 +50,7 @@ export function PageButtonGroup({
       <ButtonGroup size="sm" isAttached variant="outline">
         <Center color="gray">
           <Text marginRight={"10px"} fontSize={"sm"}>
-            Page {Math.floor(range / 3) + 1} of {numOfPages}
+            Page {page} of {numOfPages}
           </Text>
 
           <PageLeftButton click={leftPage} />
@@ -40,7 +60,8 @@ export function PageButtonGroup({
             p={2}
             width="auto"
             size="sm"
-            value={`${Math.floor(range / 3) + 1}`}
+            value={page}
+            onChange={changePage}
           />
           <PageRightButton click={rightPage} />
         </Center>
