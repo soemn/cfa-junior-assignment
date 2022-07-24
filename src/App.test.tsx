62ee4f29d1f72d50b487d61efffa8b200e4a8bfa
berniceyeo <berniceyeosuping@gmail.com>
2022-07-24 13:42:59 +0800

    refractor and edit errors in code

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders carousel component', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Carousel/i);
-  expect(linkElement).toBeInTheDocument();
-});
